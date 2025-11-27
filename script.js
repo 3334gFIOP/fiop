e38e78d1a0a9a408f8d0642ecd3e003a668b5c33
3334gFIOP-school <jackson.hauley>
2025-11-26 22:42:30 -0700

    fix button looks

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const items = document.querySelectorAll(".accordion-item");
 window.addEventListener("DOMContentLoaded", () => {
     const first = items[0].querySelector(".accordion-content");
-    first.style.maxHeight = first.scrollHeight + "px";
+    first.style.height = first.scrollHeight + "px";
     first.classList.add("open");
 });
+
 items.forEach(item => {
     const header = item.querySelector(".accordion-header");
     const content = item.querySelector(".accordion-content");
@@ -12,11 +13,12 @@ items.forEach(item => {
         const isOpen = content.classList.contains("open");
 
         document.querySelectorAll(".accordion-content").forEach(c => {
-            c.style.maxHeight = null;
+            c.style.height = "0";
             c.classList.remove("open");
         });
+
         if (!isOpen) {
-            content.style.maxHeight = content.scrollHeight + "px";
+            content.style.height = content.scrollHeight + "px";
             content.classList.add("open");
         }
     });
