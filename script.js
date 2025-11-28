8ca8e237c2a7333c51faeffffd36ab3ade3847c0
3334gFIOP-school <jackson.hauley>
2025-11-28 10:08:41 -0700

    fix bottom margin

--- script.js
+++ script.js
@@ -3,18 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
     const panels = document.querySelectorAll(".panel");
 
     panels[0].style.maxHeight = panels[0].scrollHeight + "px";
+    panels[0].classList.add("open");
 
     accordions.forEach((btn, index) => {
         btn.addEventListener("click", () => {
             panels.forEach((p, i) => {
-                if (i !== index) p.style.maxHeight = null;
+                if (i !== index) {
+                    p.style.maxHeight = null;
+                    p.classList.remove("open");
+                }
             });
 
             const panel = btn.nextElementSibling;
+
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
+                panel.classList.remove("open");
             } else {
                 panel.style.maxHeight = panel.scrollHeight + "px";
+                panel.classList.add("open");
             }
         });
     });
